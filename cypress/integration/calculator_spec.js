75c82bfd7420e66787905a0f4e7b60e19e419780
chipo <chipomapondera@gmail.com>
2019-07-28 11:00:42 +0100

    cypress testing- with clear functions

--- cypress/integration/calculator_spec.js
+++ cypress/integration/calculator_spec.js
@@ -1,39 +1,83 @@
-/*describe('My First Test', function() {
+//First block of code is from the Cypress tutorial
+
+
+/*
+describe('My Test 1', function() {
 	it('Does not do much!', function() {
 		expect(true).to.equal(true)
 	})
 })
 
-describe('My First Test', function() {
-	it('Does not do much!', function() {
+
+describe('A Failing Test', function() {
+	it('Shows what happens when a test fails!', function() {
 		expect(true).to.equal(false)
 	})
 })
 
-describe('My First Test', function() {
+
+describe('My Test 2', function() {
 	it('Visits the Kitchen Sink', function() {
 		cy.visit('https://example.cypress.io')
 	})
 })
 
-describe('My First Test', function() {
+
+describe('My Test 3', function() {
 	it('finds the content "type"', function() {
 		cy.visit('https://example.cypress.io')
-
 		cy.contains('type').click()
 	})
-})*/
+})
+
+
+describe('My Test 4', function() {
+	it('clicking "type" navigates to a new url', function() {
+		cy.visit('https://example.cypress.io')
+		cy.contains('type').click()
+		cy.url().should('include', 'commands/actions')
+	})
+})
+
+
+describe('My Test 5', function() {
+	it('Gets, types and asserts', function() {
+		cy.visit('https://example.cypress.io')
+		cy.contains('type').click()
+		cy.url().should('include', 'commands/actions')
+		cy.get('.action-email')
+		  .type('fake@email.com')
+		  .should('have.value', 'fake@email.com')
+	})
+})
+
+
+describe('My Test 6', function() {
+	it('clicking "type" shows the right headings', function() {
+		cy.visit('https://example.cypress.io')
+		cy.pause()
+		cy.contains('type').click()
+		cy.url().should('include', 'commands/actions')
+		cy.get('.action-email')
+		  .type('fake@email.com')
+		  .should('have.value', 'fake@email.com')
+	})
+})
+*/
+
+
+//Code below is testing Calculator
 
 describe('addition', function() {
 	it('adds 2+2', function() {
 		cy.visit('/')
 		cy.contains('2').click()
 		cy.contains('+').click()
-		cy.contains('2').click()
+		cy.contains('3').click()
 		cy.contains('=').click()
 
 		cy.get('.calculator__display')
-		  .should('have.text', '4')
+		  .should('have.text', '5')
 	})
 })
 
@@ -41,9 +85,9 @@ describe('addition', function() {
 describe('subtraction', function() {
 	it('subtracts 4-2', function() {
 		cy.visit('/')
-		cy.contains('4').click()
+		cy.contains('5').click()
 		cy.contains('-').click()
-		cy.contains('2').click()
+		cy.contains('3').click()
 		cy.contains('=').click()
 
 		cy.get('.calculator__display')
@@ -69,9 +113,9 @@ describe('multiplication', function() {
 describe('division', function() {
 	it('divides 4÷2', function() {
 		cy.visit('/')
-		cy.contains('4').click()
+		cy.contains('6').click()
 		cy.contains('÷').click()
-		cy.contains('2').click()
+		cy.contains('3').click()
 		cy.contains('=').click()
 
 		cy.get('.calculator__display')
@@ -115,5 +159,48 @@ describe('decimal after equals', function() {
 		  .should('have.text', '0.')
 	})
 })
+
+
+describe('clear with AC', function() {
+	it('clears all previous inputs', function() {
+		cy.visit('/')
+		cy.contains('1').click()
+		cy.contains('+').click()
+		cy.contains('3').click()
+		cy.contains('CE').click()
+		cy.contains('AC').click()
+		cy.get('.calculator__display')
+		  .should('have.text', '0')
+	})
+})
+
+
+describe('clear button shows CE', function() {
+	it('clear button turns from AC to CE', function() {
+		cy.visit('/')
+		cy.contains('1').click()
+		cy.get('.AC--button')
+		  .should('have.text', 'CE')
+	})
+})
+
+
+describe('clear with CE', function() {
+	it('CE clears last input', function() {
+		cy.visit('/')
+		cy.contains('1').click()
+		cy.contains('+').click()
+		cy.contains('2').click()
+		cy.contains('+').click()
+		cy.contains('4').click()
+		cy.contains('+').click()
+		cy.contains('1').click()
+		cy.contains('CE').click()
+		cy.contains('=').click()
+		cy.get('.calculator__display')
+		  .should('have.text', '7')
+	})
+})
+
 
 
